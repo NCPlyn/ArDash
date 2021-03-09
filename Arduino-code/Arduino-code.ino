6ec5734135f93c50014537d401e35d65fc8d27ad
NCPlyn <44117369+NCPlyn@users.noreply.github.com>
2021-03-09 16:42:23 +0100

    Fix serial read

--- Arduino-code/Arduino-code.ino
+++ Arduino-code/Arduino-code.ino
@@ -8,19 +8,19 @@ void setup() {
 
 void loop() {
   if (digitalRead(2)==LOW){
-    Serial.print("1");
+    Serial.print("01");
     while(digitalRead(2)==LOW) {}
     delay(100);
   } else if (digitalRead(3)==LOW){
-    Serial.print("2");
+    Serial.print("02");
     while(digitalRead(3)==LOW) {}
     delay(100);
   } else if (digitalRead(4)==LOW){
-    Serial.print("3");
+    Serial.print("03");
     while(digitalRead(4)==LOW) {}
     delay(100);
   } else if (digitalRead(5)==LOW){
-    Serial.print("4");
+    Serial.print("04");
     while(digitalRead(5)==LOW) {}
     delay(100);
   }
