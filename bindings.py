a3b03bd2ecfed55ef896647971974395d69632eb
NCPlyn <44117369+NCPlyn@users.noreply.github.com>
2021-03-09 19:13:09 +0100

    Fixed example

--- bindings.py
+++ bindings.py
@@ -2,15 +2,16 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 import os
 
-def ifFunc(xx, bcolors):
-    if(xx == 1):
+def ifFunc(serIN, bcolors):
+    if(serIN == "1"):
         keyboard.tap(Key.pause)
-
-    if(serIN == 2):
+        print(bcolors.CYAN+"[INFO]"+bcolors.ENDC+" Mute/Unmute discord")
+    if(serIN == "2"):
         os.system('taskkill /f /im Teams.exe')
-    if(serIN == 3):
+        print(bcolors.CYAN+"[INFO]"+bcolors.ENDC+" Killed Spotify")
+    if(serIN == "3"):
         os.system('start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile2')
-    if(serIN == 4):
+        print(bcolors.CYAN+"[INFO]"+bcolors.ENDC+" GPU OC done")
+    if(serIN == "4"):
         os.system('start "" "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -Profile1')
         print(bcolors.CYAN+"[INFO]"+bcolors.ENDC+" GPU Stock done")
-
